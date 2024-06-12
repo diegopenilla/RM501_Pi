6bfa183992c3ef8b11535bc084094dbcf154859a
diegopenilla <42318333+diegopenilla@users.noreply.github.com>
2024-06-12 23:30:35 +0200

    fixed error

--- rm501_lib/roboclaw_python/read_signal.py
+++ rm501_lib/roboclaw_python/read_signal.py
@@ -9,10 +9,9 @@ try:
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
-            print(f"Received message: {line}")
-
-            robot_arm = pick_paper.RobotArm("/dev/RM501", 115200)
-            robot_arm.initialize_and_execute("kpr_positions.csv")
+            if line == "coin":
+                robot_arm = pick_paper.RobotArm("/dev/RM501", 115200)
+                robot_arm.initialize_and_execute("kpr_positions.csv")
 
             time.sleep(10)
 except KeyboardInterrupt:
