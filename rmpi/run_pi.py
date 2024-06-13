f428bc18593f3df740cf268725ec8ea2425025fe
diegopenilla <42318333+diegopenilla@users.noreply.github.com>
2024-06-13 16:51:20 +0200

    fixed error

--- rmpi/run_pi.py
+++ rmpi/run_pi.py
@@ -11,10 +11,5 @@ formatted_time = current_time.strftime("%Y%m%d_%H%M%S")
 # Create a filename using the formatted time
 filename = f"./results/{formatted_time}.txt"
 
-# Create and open the file
-with open(filename, 'w') as file:
-    file.write("This file was created at " + current_time.strftime("%Y-%m-%d %H:%M:%S"))
-
-print(f"File {filename} has been created.")
 
 time.sleep(600)
