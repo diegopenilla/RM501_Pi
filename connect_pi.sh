b7353414acdc2312c4af3ce3c1e5ca1038d7c207
diegopenilla <42318333+diegopenilla@users.noreply.github.com>
2024-06-13 16:52:52 +0200

    fixed error

--- connect_pi.sh
+++ connect_pi.sh
@@ -1,8 +1,2 @@
 # when connecting run
 ssh pi@192.168.178.21
-# pipi20
-cd RM501_Pi/
-source env/bin/activate
-cd rm501_lib/roboclaw_python/
-streamlit run app_csv_wrist.py
-
---
+++ run_app_pi.sh
@@ -0,0 +1,5 @@
+# pipi20
+cd RM501_Pi/
+source env/bin/activate
+cd rm501_lib/roboclaw_python/
+streamlit run app_csv_wrist.py
